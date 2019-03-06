b3137b6d15b04b635a20d8d60edeea9f3f501100
Eli Howey <elihowey@gmail.com>
2019-03-06 16:40:51 -0600

    Change WPM constant to 200

--- scripts/blog-post-length.js
+++ scripts/blog-post-length.js
@@ -10,7 +10,7 @@
  * adult to read a page's article
  */
 function getEstimatedTimeToRead() {
-  const WORDS_PER_MINUTE = 275; // Average adult reading speed
+  const WORDS_PER_MINUTE = 200; // Average adult reading speed
   const article = document.getElementsByTagName('article')[0];
   const numWords = article.innerText.split(' ').length;
   return Math.round(numWords / WORDS_PER_MINUTE);

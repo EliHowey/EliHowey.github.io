6232ba08f2102f14ae921bd1f7010544d357aa82
Eli Howey <elihowey@gmail.com>
2019-03-05 18:08:26 -0600

    Add blog post reading time estimates

---
+++ scripts/blog-post-length.js
@@ -0,0 +1,30 @@
+/**
+ * blog-post-length.js
+ * Blog post length estimation
+ * Written by Eli Howey
+ * Copyright 2019
+ */
+
+/**
+ * Estimates the length of time it will take the average
+ * adult to read a page's article
+ */
+function getEstimatedTimeToRead() {
+  const WORDS_PER_MINUTE = 275; // Average adult reading speed
+  const article = document.getElementsByTagName('article')[0];
+  const numWords = article.innerText.split(' ').length;
+  return Math.round(numWords / WORDS_PER_MINUTE);
+}
+
+window.addEventListener('load', () => {
+  const lengthEl = document.getElementById('blog-post-length-estimate');
+
+  if (lengthEl) {
+    const timeToRead = getEstimatedTimeToRead();
+    if (timeToRead === undefined || timeToRead === null || Number.isNaN(timeToRead)) {
+      lengthEl.remove();
+    } else {
+      lengthEl.innerText = `${timeToRead < 1 ? '<1' : timeToRead} minute read`;
+    }
+  }
+});

600295f40696ad02a6e951ef9329841537cfe823
Eli Howey <elihowey@gmail.com>
2019-10-09 22:45:25 -0400

    Abstract the relative date string calculation code

--- dist/scripts/components/blog-post-timestamp.js
+++ dist/scripts/components/blog-post-timestamp.js
@@ -1,45 +1,40 @@
 export class BlogPostTimestamp extends HTMLTimeElement {
     constructor() {
         super();
-        this.absoluteDateFormatter = new Intl.DateTimeFormat('en-US', {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
+        this.absoluteDateFormatter = new Intl.DateTimeFormat("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric"
         });
-        this.relativeDateFormatter = new Intl.RelativeTimeFormat('en-US', { numeric: 'auto' });
+        this.relativeDateFormatter = new Intl.RelativeTimeFormat("en-US", {
+            numeric: "auto"
+        });
     }
     connectedCallback() {
-        let relativeDateString;
         const postDate = Date.parse(this.dateTime);
-        const relativeDateInMinutes = (postDate - Date.now()) / (1000 * 60);
-        if (Math.abs(relativeDateInMinutes) < 60) {
-            relativeDateString = this.relativeDateFormatter.format(Math.round(relativeDateInMinutes), 'minutes');
-        }
-        else {
-            const relativeDateInHours = relativeDateInMinutes / 60;
-            if (Math.abs(relativeDateInHours) < 24) {
-                relativeDateString = this.relativeDateFormatter.format(Math.round(relativeDateInHours), 'hours');
-            }
-            else {
-                const relativeDateInDays = relativeDateInHours / 24;
-                if (Math.abs(relativeDateInDays) < 30) {
-                    relativeDateString = this.relativeDateFormatter.format(Math.round(relativeDateInDays), 'days');
-                }
-                else {
-                    const relativeDateInMonths = relativeDateInDays / 30;
-                    if (Math.abs(relativeDateInMonths) < 12) {
-                        relativeDateString = this.relativeDateFormatter.format(Math.round(relativeDateInMonths), 'months');
-                    }
-                    else {
-                        const relativeDateInYears = relativeDateInMonths / 12;
-                        relativeDateString = this.relativeDateFormatter.format(Math.round(relativeDateInYears), 'years');
-                    }
-                }
-            }
-        }
-        this.textContent = relativeDateString;
+        this.textContent = this.getRelativeDateString(postDate);
         this.title = this.absoluteDateFormatter.format(new Date(postDate));
+    }
+    getRelativeDateString(timestamp) {
+        const relativeDateInMinutes = (timestamp - Date.now()) / (1000 * 60);
+        if (Math.abs(relativeDateInMinutes) < 60) {
+            return this.relativeDateFormatter.format(Math.round(relativeDateInMinutes), "minutes");
+        }
+        const relativeDateInHours = relativeDateInMinutes / 60;
+        if (Math.abs(relativeDateInHours) < 24) {
+            return this.relativeDateFormatter.format(Math.round(relativeDateInHours), "hours");
+        }
+        const relativeDateInDays = relativeDateInHours / 24;
+        if (Math.abs(relativeDateInDays) < 30) {
+            return this.relativeDateFormatter.format(Math.round(relativeDateInDays), "days");
+        }
+        const relativeDateInMonths = relativeDateInDays / 30;
+        if (Math.abs(relativeDateInMonths) < 12) {
+            return this.relativeDateFormatter.format(Math.round(relativeDateInMonths), "months");
+        }
+        const relativeDateInYears = relativeDateInMonths / 12;
+        return this.relativeDateFormatter.format(Math.round(relativeDateInYears), "years");
     }
 }
 //# sourceMappingURL=blog-post-timestamp.js.map

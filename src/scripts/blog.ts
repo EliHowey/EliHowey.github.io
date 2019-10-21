2a048a2100dcc3c7cc432b8afa0819da5828999b
Eli Howey <elihowey@gmail.com>
2019-10-20 22:59:34 -0400

    Remove header comments from Typescript files

--- src/scripts/blog.ts
+++ src/scripts/blog.ts
@@ -1,10 +1,3 @@
-/*
-  blog.js
-  Behavior of the blog page
-  Written by Eli Howey
-  Copyright 2019
-*/
-
 import { BlogPostTimestamp } from './components/blog-post-timestamp.js';
 
 customElements.define('blog-post-timestamp', BlogPostTimestamp, {

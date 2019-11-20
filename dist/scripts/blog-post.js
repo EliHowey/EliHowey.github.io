1c7c07db29b64572d07226ada152c60c5397e4a4
Eli Howey <elihowey@gmail.com>
2019-11-20 11:34:16 -0500

    Generated files

--- dist/scripts/blog-post.js
+++ dist/scripts/blog-post.js
@@ -2,28 +2,27 @@ import { BlogPostLength } from './components/blog-post-length.js';
 import { BlogPostProgressBar } from './components/blog-post-progress-bar.js';
 import { BlogPostTimestamp } from './components/blog-post-timestamp.js';
 function renderHeadingLinks() {
-    const headings = document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]');
-    console.log(headings);
-    for (const heading of headings) {
-        const link = document.createElement('a');
-        link.href = `#${heading.id}`;
-        link.className = 'heading-permalink';
-        link.setAttribute('aria-label', 'Permalink');
-        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        svg.setAttribute('viewBox', '0 0 457.03 457.03');
-        const useEl = document.createElementNS('http://www.w3.org/2000/svg', 'use');
-        useEl.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#icon--link');
-        useEl.setAttribute('href', '#icon--link');
-        svg.appendChild(useEl);
-        link.appendChild(svg);
-        heading.appendChild(link);
-    }
+	const headings = document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]');
+	for (const heading of headings) {
+		const link = document.createElement('a');
+		link.href = `#${heading.id}`;
+		link.className = 'heading-permalink';
+		link.setAttribute('aria-label', 'Permalink');
+		const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+		svg.setAttribute('viewBox', '0 0 457.03 457.03');
+		const useEl = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+		useEl.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#icon--link');
+		useEl.setAttribute('href', '#icon--link');
+		svg.appendChild(useEl);
+		link.appendChild(svg);
+		heading.appendChild(link);
+	}
 }
 customElements.define('blog-post-timestamp', BlogPostTimestamp, {
-    extends: 'time'
+	extends: 'time'
 });
 customElements.define('blog-post-progress-bar', BlogPostProgressBar, {
-    extends: 'progress'
+	extends: 'progress'
 });
 customElements.define('blog-post-length', BlogPostLength);
 renderHeadingLinks();
